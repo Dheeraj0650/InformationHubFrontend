8e37fcd5d5fcb5a7de2582ee68aeefe743ef4e23
Dheeraj0650 <kottedheeraj@gmail.com>
2021-08-26 19:59:13 +0530

    updated authentication code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,6 +12,7 @@ import GoogleLogin from 'react-google-login';
 import GitHubLogin from 'react-login-github';
 import Cookies from 'js-cookie';
 import socialMediaAuth from '../config/auth';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {googleProvider,githubProvider,microsoftProvider} from '../config/authMethods';
 
 function TabPanel(props) {
@@ -57,12 +58,39 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FullWidthTabs(props) {
   const dispatch = useDispatch();
-  const [alert_1,changeAlert_1] = useState('');
-  const [alert_2,changeAlert_2] = useState('');
-  const [alert_3,changeAlert_3] = useState('');
-  const [alert_4,changeAlert_4] = useState('');
+  const [alert_1,changeAlert_1] = useState([false,'']);
+  const [alert_2,changeAlert_2] = useState([false,'']);
+  const [alert_3,changeAlert_3] = useState([false,'']);
+  const [alert_4,changeAlert_4] = useState([false,'']);
+  const [alert_5,changeAlert_5] = useState([false,'']);
+  const [circularProgress,setCircularProgress] = useState("static");
 
+  let verify = false;
 
+  function validateUsername(event){
+  	  var username=event.target.value;
+  	  var usernamepattern=/^[A-Za-z .]{3,15}$/;
+    	if(usernamepattern.test(username))
+    	{
+        if((event.target.name.charAt(event.target.name.length-1))==='3')
+        {
+          changeAlert_5([true,""])
+        }
+        else{
+          changeAlert_3([true,""])
+        }
+      }
+      else
+      {
+        if((event.target.name.charAt(event.target.name.length-1))==='3')
+        {
+          changeAlert_5([false,"* invalid username, username should contain only characters [A-Z][a-z] and length should be in between 3 and 15"])
+        }
+        else{
+          changeAlert_3([false,"* invalid username, username should contain only characters [A-Z][a-z] and length should be in between 3 and 15"])
+        }
+      }
+	 }
 
   function validatePassword(event) {
      var str = event.target.value;
@@ -74,23 +102,23 @@ export default function FullWidthTabs(props) {
 
     if((event.target.name.charAt(event.target.name.length-1))==='1')
     {   if(res === 1){
-          changeAlert_2("");
+          changeAlert_2([true,""]);
         }
         else{
-          changeAlert_2("* weak password A password is correct if it contains: 1) At least 1 uppercase character.2) At least 1 lowercase character.3) At least 1 digit.4) At least 1 special character.5) Minimum 8 characters.");
+          changeAlert_2([false,"* weak password A password is correct if it contains: 1) At least 1 uppercase character.2) At least 1 lowercase character.3) At least 1 digit.4) At least 1 special character.5) Minimum 8 characters."]);
           if(str===""){
-             changeAlert_2("");
+             changeAlert_2([true,""]);
           }
         }
     }
     else{
         if(res === 1){
-            changeAlert_4("");
+            changeAlert_4([true,""]);
         }
         else{
-          changeAlert_4("* invalid password ");
+          changeAlert_4([false,"* invalid password "]);
           if(str===""){
-             changeAlert_4("");
+             changeAlert_4([true,""]);
           }
         }
     }
@@ -114,23 +142,23 @@ export default function FullWidthTabs(props) {
     if((event.target.name.charAt(event.target.name.length-1))==='1')
     {
       if(res === 1){
-           changeAlert_1("");
+           changeAlert_1([true,""]);
          }
          else{
-           changeAlert_1("* invalid email ");
+           changeAlert_1([false,"* invalid email "]);
            if(str===""){
-              changeAlert_1("");
+              changeAlert_1([true,""]);
            }
          }
     }
     else{
       if(res === 1){
-         changeAlert_3("");
+         changeAlert_3([true,""]);
       }
       else{
-         changeAlert_3("* invalid email ");
+         changeAlert_3([false,"* invalid email "]);
          if(str===""){
-            changeAlert_3("");
+            changeAlert_3([true,""]);
          }
        }
     }
@@ -170,15 +198,16 @@ export default function FullWidthTabs(props) {
         var data = response.text();
         var p = Promise.resolve(data);
         p.then(function(value) {
+             setCircularProgress("static");
              if(value === "Successful"){
                  dispatch({type:'login'});
              }
              else{
                  if(method === 'login'){
-                   changeAlert_4(value);
+                   changeAlert_4([true,value]);
                  }
                  else{
-                   changeAlert_2(value);
+                   changeAlert_2([true,value]);
                  }
                  dispatch({type:'logout'});
              }
@@ -189,28 +218,35 @@ export default function FullWidthTabs(props) {
   const clickHandler = (event) => {
       event.preventDefault();
       if(event.target.username_1){
-        const username = event.target.username.value;
-        const email = event.target.username_1.value;
-        const password = event.target.password_1.value;
-        let details = {
-          'email':email,
-          'username':username,
-          'password':password
-        };
-        Users(details,'register');
+        if(alert_1[0] && alert_2[0] && alert_5[0]){
+          setCircularProgress("indeterminate");
+          const username = event.target.username_3.value;
+          const email = event.target.username_1.value;
+          const password = event.target.password_1.value;
+          let details = {
+            'email':email,
+            'username':username,
+            'password':password
+          };
+          Users(details,'register');
+        }
       }
       else{
-        const username = event.target.username_2.value;
-        const password = event.target.password_2.value;
-        let details = {
-          'username':username,
-          'password':password
-        };
-        Users(details,'login');
+        if(alert_3[0] && alert_4[0]){
+          setCircularProgress("indeterminate");
+          const username = event.target.username_2.value;
+          const password = event.target.password_2.value;
+          let details = {
+            'username':username,
+            'password':password
+          };
+          Users(details,'login');
+        }
       }
   }
 
   const clickHandlerOAuth = async (method) => {
+    setCircularProgress("indeterminate");
     const res = await socialMediaAuth(method);
     const username = res.displayName;
     const email = res.email;
@@ -230,6 +266,9 @@ export default function FullWidthTabs(props) {
       else if(method === microsoftProvider){
         Users(details,'auth/microsoft');
       }
+    }
+    else{
+      setCircularProgress("static");
     }
 
   }
@@ -260,12 +299,13 @@ export default function FullWidthTabs(props) {
         <div class="signup">
             <form onSubmit = {clickHandler}>
               <label for="chk" aria-hidden="true" class="login-label">Sign up</label>
-              <input type="name" name="username" placeholder="Username" required="" class="login-input username-2" />
-              <p className = "alert">{alert_3}</p>
+              <input onChange = {validateUsername} type="name" name="username_3" placeholder="Username" required="" class="login-input username-2" />
+              <p className = "alert">{alert_5[1]}</p>
               <input onChange={validateEmail}  type="email" name="username_1" placeholder="Email" required="" class="login-input username-1" />
-              <p className = "alert">{alert_1}</p>
+              <p className = "alert">{alert_1[1]}</p>
               <input onChange={validatePassword}  type="password" name="password_1" placeholder="Password" required="" class="login-input password-1" />
-              <p className = "alert">{alert_2}</p>
+              <p className = "alert">{alert_2[1]}</p>
+              {circularProgress==="indeterminate" && <CircularProgress color="primary" variant={circularProgress}/>}
               <button type="submit" class="hover-button">Sign up</button>
             </form>
         </div>
@@ -275,10 +315,11 @@ export default function FullWidthTabs(props) {
         <div class="login">
             <form onSubmit = {clickHandler}>
               <label for="chk" aria-hidden="true" class="login-label">Login</label>
-              <input  type="name" name="username_2" placeholder="Username" required="" class="login-input username-2" />
-              <p className = "alert">{alert_3}</p>
+              <input onChange = {validateUsername} type="name" name="username_2" placeholder="Username" required="" class="login-input username-2" />
+              <p className = "alert">{alert_3[1]}</p>
               <input onChange={validatePassword} type="password" name="password_2" placeholder="Password" required="" class="login-input password-2" />
-              <p className = "alert">{alert_4}</p>
+              <p className = "alert">{alert_4[1]}</p>
+              {circularProgress==="indeterminate" && <CircularProgress color="primary" variant={circularProgress}/>}
               <button type="submit" class="hover-button">Login</button>
             </form>
        </div>
@@ -286,7 +327,7 @@ export default function FullWidthTabs(props) {
         <TabPanel value={value} index={2} dir={theme.direction}>
             <div class="container-fluid" style={{lineHeight:"5",display:"grid",justifyContent:"center",alignItems:"center"}}>
             <a onClick = {() => clickHandlerOAuth(googleProvider)} class="btn btn-outline-danger btn-block btn-6" style={{fontSize:"1.15rem"}}><i class="fab fa-google" style={{paddingRight:"1rem",fontSize:"1.4rem"}}></i> Sign in with <b>Google</b></a>
-            <a onClick = {() => clickHandlerOAuth(microsoftProvider)} class="btn btn-outline-primary btn-block btn-4" style={{fontSize:"1.15rem"}}><i class="fab fa-facebook-square " style={{paddingRight:"1rem",fontSize:"1.4rem"}}></i> Sign in with <b>Facebook</b></a>
+            <a onClick = {() => clickHandlerOAuth(microsoftProvider)} class="btn btn-outline-success btn-block btn-4" style={{fontSize:"1.15rem"}}><i class="fab fa-microsoft" style={{paddingRight:"1rem",fontSize:"1.4rem"}}></i> Sign in with <b>Microsoft</b></a>
             <a onClick = {() => clickHandlerOAuth(githubProvider)} class="btn btn-outline-dark btn-block btn-5" style={{fontSize:"1.15rem"}}><i class="fab fa-github " style={{paddingRight:"1rem",fontSize:"1.4rem"}}></i>Sign in with <b>Github</b></a>
             </div>
         </TabPanel>
